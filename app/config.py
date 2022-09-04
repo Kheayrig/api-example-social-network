58eed8769c7f52b19cbd82942b511e0968e05d6e
Kheayrig <kheayrig@gmail.com>
2022-09-04 18:33:56 +0300

    heroku

--- app/config.py
+++ app/config.py
@@ -17,7 +17,7 @@ path = os.path.join(str(Path.cwd()), 'static')
 DATA_PATH = config('DATA_PATH', cast=str, default=path)
 
 # database
-DATABASE_URL = config('DATABASE_URL', cast=str, default='postgres://utfpmtzzdxbdij:c0575207ea21666604d6d2ab1344b2ed203cc46aaad234929f1863b5a03fe2dd@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d2fd31b8df7gta')
+DATABASE_URL = config('DATABASE_URL', cast=str, default='postgresql://utfpmtzzdxbdij:c0575207ea21666604d6d2ab1344b2ed203cc46aaad234929f1863b5a03fe2dd@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d2fd31b8df7gta')
 DATABASE_NAME = config('DATABASE_NAME', cast=str, default='d2fd31b8df7gta')
 DATABASE_USER = config('DATABASE_USER', cast=str, default='utfpmtzzdxbdi')
 DATABASE_PASSWORD = config('DATABASE_PASSWORD', cast=str, default='c0575207ea21666604d6d2ab1344b2ed203cc46aaad234929f1863b5a03fe2dd')
