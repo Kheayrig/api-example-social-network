8784cb64c4c578c1df8498da2feded40c286a39f
Катя <kheayrig@gmail.com>
2022-09-04 14:34:03 +0300

    create README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+<h2>Социальная сеть</h2>
+
+Реализация REST-API социальной сети на фреймворке fastapi
+
+<h2>Установка</h2>
+Требуется наличие базы данных PostgreSQL или наличие доступа к ней
+
+<b>Ubuntu</b>:
+
+Установка python3:
+sudo apt update
+sudo apt install python3 python3-pip
+Установка зависимостей:
+pip3 install -r requirements.txt
+
+<h2>Запуск</h2>
+
+python3 -m uvicorn main:app
+
+<h2>Настройка</h2>
+
+configs - Конфиги подключения -> #server, #database, #data_path
+
+
+<h2>Пример работы</h2>
+
+Посмотреть, как будет функционировать сервис, можно тут:
+
+.............
--- app/config.py
+++ app/config.py
@@ -2,18 +2,24 @@ from starlette.config import Config
 
 config = Config('.env')
 
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ALGORITHM = 'HS256'
+SECRET_KEY = 'aerfudiohkj439uji43kfu8oi32f9jo3r29j0dej09w23rewafzds'
+
+#data_path
+"""
+path to media storage
+"""
+DATA_PATH = config('DATA_PATH', cast=str, default=r'D:/static/')
+
+#server
 HOST = config('DATABASE_HOST', cast=str, default='127.0.0.1')
 PORT = config('PORT', cast=int, default=8000)
 
+#database
 DATABASE_URL = config('AESN_DATABASE_URL', cast=str,
                       default='postgresql://postgres:root@localhost:32700/ae-social-network')
 DATABASE_NAME = config('DATABASE_NAME', cast=str, default='ae-social-network')
 DATABASE_USER = config('DATABASE_USER', cast=str, default='postgres')
 DATABASE_PASSWORD = config('DATABASE_PASSWORD', cast=str, default='root')
 DATABASE_PORT = config('DATABASE_PORT', cast=str, default='32700')
-
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
-ALGORITHM = 'HS256'
-SECRET_KEY = 'aerfudiohkj439uji43kfu8oi32f9jo3r29j0dej09w23rewafzds'
-
-DATA_PATH = config('DATA_PATH', cast=str, default=r'D:/static/')
